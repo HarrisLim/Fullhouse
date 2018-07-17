5cc87db128727760726322b33dffa61dc3a5be6a
plannist <plannist19@gmail.com>
2018-07-17 17:33:54 +0900

    첫번째 변경 사항입니다. 폼.xml json view maven 추가!! 중요!!

---
+++ src/main/webapp/resources/kanu/js/map.js
@@ -0,0 +1,395 @@
+/**
+ * 
+ */
+    // 매물 종류
+	// 월세
+	$("#month_rent").click( function () {
+		  var arc = $("#month_rent").text();
+		  $("#kind_of_sale").empty().append(arc);
+	});
+    // 전세or월세
+	$("#engage_or_rent").click( function () {
+		  var arc = $("#engage_or_rent").text();
+		  $("#kind_of_sale").empty().append(arc);
+	});
+    // 전세
+	$("#engage").click( function () {
+		  var arc = $("#engage").text();
+		  $("#kind_of_sale").empty().append(arc);
+	});
+    // 매매
+	$("#sale").click( function () {
+		  var arc = $("#sale").text();
+		  $("#kind_of_sale").empty().append(arc);
+	});
+	
+	// 거래 종류
+    // 전체
+	$("#all").click( function () {
+		  var arc = $("#all").text();
+		  $("#kind_of_trade").empty().append(arc);
+	});
+    // 중개
+	$("#estate_agent").click( function () {
+		  var arc = $("#estate_agent").text();
+		  $("#kind_of_trade").empty().append(arc);
+	});
+    // 직거래
+	$("#direct_deal").click( function () {
+		  var arc = $("#direct_deal").text();
+		  $("#kind_of_trade").empty().append(arc);
+	});
+
+	// 방종류
+    // 전체
+	$("#room_count_all").click( function () {
+		  var arc = $("#room_count_all").text();
+		  $("#kind_of_room").empty().append(arc);
+	});
+    // 원룸
+	$("#room_count_1").click( function () {
+		  var arc = $("#room_count_1").text();
+		  $("#kind_of_room").empty().append(arc);
+	});
+    // 1.5룸
+	$("#room_count_1.5").click( function () {
+		  var arc = $("#room_count_1.5").text();
+		  $("#kind_of_room").empty().append(arc);
+	});
+    // 투룸
+	$("#room_count_2").click( function () {
+		  var arc = $("#room_count_2").text();
+		  $("#kind_of_room").empty().append(arc);
+	});
+    // 쓰리룲
+	$("#room_count_3").click( function () {
+		  var arc = $("#room_count_3").text();
+		  $("#kind_of_room").empty().append(arc);
+	});
+    // 오피스텔
+	$("#room_count_office").click( function () {
+		  var arc = $("#room_count_office").text();
+		  $("#kind_of_room").empty().append(arc);
+	});
+    // 아파트
+	$("#room_count_appart").click( function () {
+		  var arc = $("#room_count_appart").text();
+		  $("#kind_of_room").empty().append(arc);
+	});
+	/////////////////////////////////////////////////////////////
+	//보증금 가격
+    // 0 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 500 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 1000 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 2000 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 3000 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 4000 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 5000 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 6000 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 7000 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 8000 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 9000 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 1억 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 무제한 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+	
+    // 0 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 500 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 1000 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 2000 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 3000 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 4000 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 5000 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 6000 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 7000 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 8000 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 9000 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 1억 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 무제한 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+	
+	//월세 가격
+    // 10 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 20 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 30 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 40 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 50 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 60 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 70 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 80 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 90 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 100 만원 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 무제한 부터
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+	
+    // 0 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 10 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 20 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 30 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 40 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 50 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 60 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 70 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 80 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 90 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 100 만원 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 무제한 까지
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+	
+	// 추가 옵션
+    // 주차가능
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 반려동물 가능
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 단기 임대
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 5평 이하
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 5~10평 이하
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 10평 이상
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 반지층
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 저층(1~3층)
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 중층(4~6층)
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 고층(7층~19층)
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 초고층(20층 이상)
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+    // 옥탑
+	$("#dropdown-item_1").click( function () {
+		  var arc = $("#dropdown-item_1").text();
+		  $("#navbar_main_dropdown_1").empty().append(arc);
+	});
+
+	
+	
+    
